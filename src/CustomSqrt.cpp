7e8d561a71921b8df4f6c7b074f7e808c9b1a562
QuickestHenry <henrypaulschaar@gmail.com>
2025-09-06 22:22:18 +0200

    Adding Implement Comments

--- src/CustomSqrt.cpp
+++ src/CustomSqrt.cpp
@@ -5,4 +5,9 @@
 #else
 #define EXPORT
 #endif
-EXPORT double CustomSqrt(double input) {}
+EXPORT double CustomSqrt(unsigned int input_numerator, int input_denominator) {
+    // use the following Sqrt Approximation, lets use a first approximation, we'll use 4 as Starting Value, the starting value will be defined as x/y currently, so x/y=4, which means for now we heave x=8 and y=2
+    // We will create the sqrt of Input_numerator/input_denominator, which we will call z_1/z_2 in the following Math Functions
+    // We will calculate (z_1/z_2)/(x/y), which is (z_1/z_2)*(y/x), which is also (z_1*x)+(z_2*y), then we will add it to x/y, and half the whole thing, so we will have (z_1*x)+(z_2*y)+(x/y), we will use a/b * c/d = (a*d+c*b)/(b*d)
+    // TODO: Finish Explanation
+}
